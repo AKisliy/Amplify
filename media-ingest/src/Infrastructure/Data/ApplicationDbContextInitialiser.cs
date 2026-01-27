ac1d9e56a93483135aee242696c039b05153ba70
AKisliy <lekha.a-kisel@yandex.ru>
2026-01-27 13:50:27 +0300

    Add seed data + docker-compose

--- media-ingest/src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
+++ media-ingest/src/Infrastructure/Data/ApplicationDbContextInitialiser.cs
@@ -1,3 +1,5 @@
+using MediaIngest.Application.Common.Interfaces;
+using MediaIngest.Domain.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,13 +18,15 @@ public static class InitialiserExtensions
         if (app.Environment.IsDevelopment())
         {
             await initialiser.InitialiseAsync();
+            await initialiser.TrySeedAsync();
         }
     }
 }
 
 public class ApplicationDbContextInitialiser(
     ILogger<ApplicationDbContextInitialiser> logger,
-    ApplicationDbContext context)
+    ApplicationDbContext context,
+    IFileStorage fileStorage)
 {
     public async Task InitialiseAsync()
     {
@@ -36,6 +40,48 @@ public class ApplicationDbContextInitialiser(
         {
             logger.LogError(ex, "An error occurred while initialising the database.");
             throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+        try
+        {
+            await SeedAsync();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while seeding the database.");
+        }
+    }
+
+    private async Task SeedAsync()
+    {
+        // Seed, if necessary
+        if (!context.MediaFiles.Any())
+        {
+            var basePath = AppContext.BaseDirectory;
+            var imagePath = Path.Combine(basePath, "assets", "samples", "sample-image.jpg");
+
+            var imageFile = File.OpenRead(imagePath);
+
+            var mediaFileId = new Guid("5ee06455-ef52-453f-8cb4-33a028567c28");
+            var mediaFile = new MediaFile
+            {
+                Id = mediaFileId,
+                FileKey = "sample-file-key",
+                OriginalFileName = "sample-image.jpg",
+                ContentType = "image/jpeg",
+                FileSize = imageFile.Length
+            };
+
+            context.MediaFiles.Add(mediaFile);
+
+            await fileStorage.SaveFileAsync(imageFile, mediaFile.OriginalFileName);
+
+            await context.SaveChangesAsync();
+
+            logger.LogInformation("Seeded database with sample media file with ID: {MediaFileId}", mediaFileId);
         }
     }
 }
