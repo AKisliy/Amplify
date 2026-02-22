7d3217307f41fbcdbeac07830611142bc0fd31be
AKisliy <lekha.a-kisel@yandex.ru>
2026-02-22 14:26:48 +0300

    Refactoring of auth in Publisher

--- publisher/src/Infrastructure/DependencyInjection.cs
+++ publisher/src/Infrastructure/DependencyInjection.cs
@@ -35,6 +35,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.Extensions.Configuration;
+using Publisher.Infrastructure.Auth;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -201,58 +202,6 @@ public static class DependencyInjection
         });
 
         return services;
-    }
-
-    private static IHostApplicationBuilder AddAuth(this IHostApplicationBuilder builder)
-    {
-        var services = builder.Services;
-        var configuration = builder.Configuration;
-
-        var jwtOptions = new JwtOptions()
-        {
-            Issuer = "",
-            Audience = ""
-        };
-
-        configuration.GetSection(JwtOptions.ConfigurationSection).Bind(jwtOptions);
-
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
-
-                if (isDevelopment)
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateAudience = false,
-                        ValidateIssuer = false,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("ЭТО_МОЙ_СУПЕР_СЕКРЕТНЫЙ_КЛЮЧ_ДЛЯ_ТЕСТОВ_12345")),
-                        NameClaimType = ClaimTypes.NameIdentifier
-                    };
-                }
-                else
-                {
-                    options.Authority = jwtOptions.Issuer;
-                    options.RequireHttpsMetadata = false;
-
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuers = [jwtOptions.Issuer],
-
-                        ValidateAudience = true,
-                        ValidAudience = jwtOptions.Audience,
-
-                        ValidateIssuerSigningKey = true,
-                    };
-
-                }
-            });
-
-        return builder;
     }
 }
 
