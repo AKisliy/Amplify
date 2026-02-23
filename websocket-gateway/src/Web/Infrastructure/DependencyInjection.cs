0e2fdfb00c67660e09c6fa4f8fdad9e113b6284e
AKisliy <lekha.a-kisel@yandex.ru>
2026-02-23 15:33:32 +0300

    Add Cors policy to ws-gateway

--- websocket-gateway/src/Web/Infrastructure/DependencyInjection.cs
+++ websocket-gateway/src/Web/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using WebSocketGateway.Infrastructure.Auth;
+using WebSocketGateway.Web.Configuration;
+using WebSocketGateway.Web.Configuration.Extensions;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -6,6 +8,37 @@ public static class DependencyInjection
 {
     public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
     {
+        var services = builder.Services;
+
+        services.AddInfrastructureOptions();
+
         builder.AddAuth();
+        builder.AddCorsUsage();
+    }
+
+    private static IServiceCollection AddInfrastructureOptions(this IServiceCollection services)
+    {
+        services.AddOptionsWithFluentValidation<CorsOptions>(CorsOptions.SectionName);
+
+        return services;
+    }
+
+    private static void AddCorsUsage(this IHostApplicationBuilder builder)
+    {
+        var corsOptions = new CorsOptions()
+        {
+            DefaultPolicyName = ""
+        };
+
+        builder.Configuration.GetSection(CorsOptions.SectionName).Bind(corsOptions);
+
+        Guard.Against.NullOrEmpty(corsOptions.DefaultPolicyName, message: "Cors policy name is not set");
+
+        builder.Services.AddCors(options => options.AddPolicy(
+            corsOptions.DefaultPolicyName,
+            builder => builder.WithOrigins([.. corsOptions.AllowedOrigins])
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()));
     }
 }
