9b0d5afd047fe3b7f93fede6efd714b21939d53f
AKisliy <lekha.a-kisel@yandex.ru>
2026-02-27 16:59:03 +0300

    Change auto-publishing to use existing

---
+++ publisher/src/Infrastructure/Data/Migrations/20260227134821_AddProcessedFieldToMediaPost.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Publisher.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProcessedFieldToMediaPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "processed_in_auto_list",
+                schema: "publisher",
+                table: "media_posts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "processed_in_auto_list",
+                schema: "publisher",
+                table: "media_posts");
+        }
+    }
+}
