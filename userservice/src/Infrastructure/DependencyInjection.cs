8508f74fb9f1d0334eb9c5c626fb17ac1c8dfb62
AKisliy <lekha.a-kisel@yandex.ru>
2026-03-06 21:43:46 +0300

    Add data protection keys + fix mail options

--- userservice/src/Infrastructure/DependencyInjection.cs
+++ userservice/src/Infrastructure/DependencyInjection.cs
@@ -17,6 +17,7 @@ using UserService.Application.Common.Interfaces.Clients;
 using UserService.Infrastructure.Clients;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.HttpOverrides;
 using UserService.Infrastructure.Mail;
 using Npgsql;
@@ -39,6 +40,10 @@ public static class DependencyInjection
 
         builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
         builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        builder.Services.AddDataProtection()
+            .PersistKeysToDbContext<ApplicationDbContext>()
+            .SetApplicationName("AmplifyUserService");
 
         builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
         {
@@ -78,6 +83,7 @@ public static class DependencyInjection
         services.AddOptions<CorsOptions>().BindConfiguration(CorsOptions.SectionName);
         services.AddOptions<InternalUrlsOptions>().BindConfiguration(InternalUrlsOptions.SectionName);
         services.AddOptions<UserserviceOptions>().BindConfiguration(UserserviceOptions.SectionName);
+        services.AddOptions<MailOptions>().BindConfiguration(MailOptions.SectionName);
     }
 
     private static void AddAuth(this IHostApplicationBuilder builder)
