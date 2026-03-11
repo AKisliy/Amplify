bfa5ffb7dc199abe9f8215299e8d3eceff4b6917
AKisliy <lekha.a-kisel@yandex.ru>
2026-03-11 20:12:12 +0300

    Add validation of file extensions in ImportFromGoogleStorageDtoValidator

--- media-ingest/src/Application/Media/Commands/ImportFromGoogleStorage/ImportFromGoogleStorageValidator.cs
+++ media-ingest/src/Application/Media/Commands/ImportFromGoogleStorage/ImportFromGoogleStorageValidator.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Options;
+
 namespace MediaIngest.Application.Media.Commands.ImportFromGoogleStorage;
 
 internal class ImportFromGoogleStorageValidator : AbstractValidator<ImportFromGoogleStorageCommand>
@@ -18,12 +20,41 @@ internal class ImportFromGoogleStorageDtoValidator : AbstractValidator<ImportFro
         "video/mp4",
         "video/webm"];
 
+    private static readonly string[] AllowedFileExtensions = [
+        "jpg",
+        "jpeg",
+        "png",
+        "webp",
+        "mp4",
+        "webm"];
+
+
     public ImportFromGoogleStorageDtoValidator()
     {
         RuleFor(x => x.GsUri)
             .NotEmpty()
             .Must(uri => uri.StartsWith("gs://"))
             .WithMessage("Invalid Google Storage URI.");
+
+        RuleFor(x => x.GsUri)
+            .Must(uri =>
+            {
+                var parts = uri.Substring(5).Split('/', 2);
+                return parts.Length == 2;
+            })
+            .WithMessage("URI must be in the format gs://{BucketName}/path/to/file and match the configured bucket.");
+
+        RuleFor(x => x.GsUri)
+            .Must(uri =>
+            {
+                var parts = uri.Substring(5).Split('/', 2);
+                var bucketName = parts[0];
+                var fileKey = parts[1];
+
+                var extension = Path.GetExtension(fileKey).TrimStart('.').ToLower();
+                return AllowedFileExtensions.Contains(extension);
+            })
+            .WithMessage("Unsupported file type. Allowed types: jpg, jpeg, png, webp, mp4, webm.");
 
         RuleFor(x => x.ContentType)
             .Must(ct => AllowedContentTypes.Contains(ct))
