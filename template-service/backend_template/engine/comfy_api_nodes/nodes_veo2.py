30390874751b238a00c5a558756207b6bf8beecb
AKisliy <lekha.a-kisel@yandex.ru>
2026-04-04 10:30:23 +0300

    REmove preview model from veo nodes

--- template-service/backend_template/engine/comfy_api_nodes/nodes_veo2.py
+++ template-service/backend_template/engine/comfy_api_nodes/nodes_veo2.py
@@ -26,8 +26,8 @@ import base64
 AVERAGE_DURATION_VIDEO_GEN = 32
 MODELS_MAP = {
     "veo-2.0-generate-001": "veo-2.0-generate-001",
-    "veo-3.1-generate": "veo-3.1-generate-preview",
-    "veo-3.1-fast-generate": "veo-3.1-fast-generate-preview",
+    "veo-3.1-generate": "veo-3.1-generate-001",
+    "veo-3.1-fast-generate": "veo-3.1-fast-generate-001",
     "veo-3.0-generate-001": "veo-3.0-generate-001",
     "veo-3.0-fast-generate-001": "veo-3.0-fast-generate-001",
 }
