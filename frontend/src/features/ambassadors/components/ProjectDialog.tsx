1fcfd50eaab65a51224f813f8f14a72d7a3d4329
AKisliy <lekha.a-kisel@yandex.ru>
2026-04-27 15:47:33 +0300

    Change default value from empty string to undefined

--- frontend/src/features/ambassadors/components/ProjectDialog.tsx
+++ frontend/src/features/ambassadors/components/ProjectDialog.tsx
@@ -45,7 +45,7 @@ export function ProjectDialog({
         defaultValues: {
             name: "",
             description: "",
-            photo: "",
+            photo: undefined,
         },
     });
 
